62366e22cc1288fbba45b1c52fc24d7a970dac51
OlivSY <94442485+OlivSY@users.noreply.github.com>
2022-10-20 18:03:59 +0800

    Create view.java

---
+++ view.java
@@ -0,0 +1,11 @@
+public class view{
+    private void getCmd() {
+        // To get users’ command in the command line.
+    }
+    private void print () {
+        // To print out the map and the status of the whole game. 
+    }
+    private void win(){
+        // If a user wins, show the win status.
+    }
+}
