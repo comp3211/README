ca44bf9d5ccaa10098ac51f04f65a6dffcdf73a5
OlivSY <94442485+OlivSY@users.noreply.github.com>
2022-10-20 18:02:42 +0800

    Update GameMap.java

--- GameMap.java
+++ GameMap.java
@@ -1,6 +1,20 @@
 public class GameMap{
-  
-  private static final int COL = 7;
-  private static final int ROW = 9;
-  
+    // A public class to include all the chess board functions in it.
+    public int chessBoard[][] map //To set a chess board like how big is the size of the chess board.
+    
+    private boolean island () {
+        //Check if the piece is on the land
+    }
+    private boolean isWater(){
+        //Check if an animal chess is in the water
+    }
+    private boolean isDen(string animal) {
+        //Check if an animal is on own den or the opponent den.
+    }
+    private boolean isTrap(string animal) {
+        //Check if the animal is trapped 
+    }
+    private void getMap() {
+        //Get the map of the Jungle game
+    }
 }
