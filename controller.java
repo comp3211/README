c84205075c67dbe300142472b1949efe94008700
OlivSY <94442485+OlivSY@users.noreply.github.com>
2022-10-20 18:01:24 +0800

    Create controller.java

---
+++ controller.java
@@ -0,0 +1,5 @@
+public class controller{
+    private void parseCmd() {
+        // A function in Controller. To parse the command users entered and pass these interactions to functions in Model and View.
+    }
+}
