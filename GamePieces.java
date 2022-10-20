cdf329e5ddbd02647a8194fd67d37dec4d516937
OlivSY <94442485+OlivSY@users.noreply.github.com>
2022-10-20 18:03:19 +0800

    Create GamePieces.java

---
+++ GamePieces.java
@@ -0,0 +1,38 @@
+public class GamePieces{
+    // A public class to include all the chess pieces functions in it.
+    public int chessPieces[][] pieces // To set what chesses are in the chess board, like what animals.
+    
+    private boolean isRat () {
+        // Check if the chess piece is a rat
+    }
+    private boolean isElephant(string animal) {
+        // Check if the chess piece is a elephant
+    }
+    private boolean isCat(string animal){
+        // Check if the chess piece is a cat
+    }
+    private boolean isWolf(string animal){
+        // Check if the chess piece is a wolf
+    }
+    private boolean isLeopard(string animal){
+        // Check if the chess piece is a leopard
+    }
+    private boolean isTiger(string animal){
+        // Check if the chess piece is a tiger
+    }
+    private boolean isLion(string animal){
+        // Check if the chess piece is a lion
+    }
+    private boolean isDog(string animal){
+        // Check if the chess piece is a dog
+    }
+    private void getPieces() {
+        // Get the type of a piece
+    }
+    private int getCoordByAnimal(string animal) {
+        // Given the animal, get the coordinate of it
+    }
+    private String getAnimalByCoord(int x, int y) {
+        // Given the coordinate, know what animal is on that coordinate
+    }
+}
