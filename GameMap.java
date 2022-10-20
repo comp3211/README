2d713598a66de056711cb1c703ab0c36957c8586
n\t\n\t <75901828+Nie-Tianyi@users.noreply.github.com>
2022-10-20 22:33:03 +0800

    Update GameMap.java

--- GameMap.java
+++ GameMap.java
@@ -1,6 +1,6 @@
 public class GameMap{
     // A public class to include all the chess board functions in it.
-    private int[][] map //To set a chess board like how big is the size of the chess board.
+    private int[][] map; //To set a chess board like how big is the size of the chess board.
     
     
     private boolean island () {
